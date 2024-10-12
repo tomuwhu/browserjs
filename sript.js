dbaef9d0c862979a00aecf19bb3321889908e99c
Dr. Németh Tamás <tomuwhu@gmail.com>
2024-10-12 09:00:16 +0200

    rossz verzió

--- sript.js
+++ sript.js
@@ -1,10 +1,9 @@
 const n = 8
 window.onload = () => {
-    document.write(`<table>${
-        Array.from({length: n}, (_, i) => `
-            <tr>${Array.from({length: n}, (_, j) => `
-                <td>${i} - ${j}</td>
-            `).join("")}</tr>
-        `).join("")
-    }</table>`)
+    document.write(`<table></table>`);
+    for (let j = 0; j < n; j++) {
+        document.querySelector("table").innerHTML += `<tr>${
+            Array.from({length: n}, (_, i) => `<td>${j} - ${i}</td>`).join("")
+        }</tr>`;
+    }
 }
