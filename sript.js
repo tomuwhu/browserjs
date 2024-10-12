3cb7e487fecf289e6baf960f8980107c14456cb7
Dr. Németh Tamás <tomuwhu@gmail.com>
2024-10-12 09:01:34 +0200

    függvényes

--- sript.js
+++ sript.js
@@ -1,9 +1,6 @@
-const n = 8
 window.onload = () => {
-    document.write(`<table></table>`);
-    for (let j = 0; j < n; j++) {
-        document.querySelector("table").innerHTML += `<tr>${
-            Array.from({length: n}, (_, i) => `<td>${j} - ${i}</td>`).join("")
-        }</tr>`;
-    }
+
+}
+f = () => {
+    alert("Hello World!");
 }
