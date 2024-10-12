014c376693acbd2bb073951cef2b45602d89d9ff
Dr. Németh Tamás <tomuwhu@gmail.com>
2024-10-12 09:06:02 +0200

    .

--- sript.js
+++ sript.js
@@ -1,6 +1,10 @@
+const n = 8
 window.onload = () => {
-
-}
-f = () => {
-    alert("Hello World!");
+    document.getElementById("table").innerHTML = (`<table>${
+        Array.from({length: n}, (_, i) => `
+            <tr>${Array.from({length: n}, (_, j) => `
+                <td>${i} - ${j}</td>
+            `).join("")}</tr>
+        `).join("")
+    }</table>`)
 }
